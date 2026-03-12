a5f88ba60fff274d720c648069716711d3e365bf
Igor Alves Rodrigues <igor.tub18@gmail.com>
2026-03-12 17:52:54 -0300

    feat: adjust onesignal

--- onesignal-init.js
+++ onesignal-init.js
@@ -4,11 +4,10 @@ OneSignal.push(async function () {
 
   await OneSignal.init({
     appId: "b4765cb0-2721-47bc-88ce-0610578ee799",
+  });
 
-    notifyButton: {
-      enable: false
-    }
-
+  OneSignal.on('subscriptionChange', function (isSubscribed) {
+    console.log("Subscription:", isSubscribed);
   });
 
 });
