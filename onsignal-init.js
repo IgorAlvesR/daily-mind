7bd1d285f5c20bdcc17450226591c48813bed564
Igor Alves Rodrigues <igor.tub18@gmail.com>
2026-03-12 17:46:34 -0300

    feat: adjust config onsignal

--- onsignal-init.js
+++ onsignal-init.js
@@ -3,7 +3,12 @@ window.OneSignal = window.OneSignal || [];
 OneSignal.push(async function () {
 
   await OneSignal.init({
-    appId: "b4765cb0-2721-47bc-88ce-0610578ee799"
+    appId: "b4765cb0-2721-47bc-88ce-0610578ee799",
+
+    notifyButton: {
+      enable: false
+    }
+
   });
 
 });
